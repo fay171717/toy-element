1039ab86f10edf0a2ed6c70b1a58e3cadcfa5e56
fay171717 <15777063880@163.com>
2024-09-10 16:25:35 +0800

    add Button funcionality

--- packages/components/Button/Button.vue
+++ packages/components/Button/Button.vue
@@ -1,8 +1,54 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import type { ButtonProps,ButtonEmits,ButtonInstance } from "./types";
+import {throttle} from 'lodash-es'
+
 defineOptions({
   name: "ErButton",
 });
+const props = withDefaults(defineProps<ButtonProps>(), {
+  tag: "button",
+  nativeType: "button",
+  useThrottle:true,
+  throttleDuration:500 //节流的间隔
+});
+
+const emits = defineEmits<ButtonEmits>();
+const slots = defineSlots();
+
+const _ref = ref<HTMLButtonElement>();
+
+const handleBtnClick = (e:MouseEvent)=> emits('click',e);
+const handleBtnClickThrottle = throttle(handleBtnClick,props.throttleDuration);
+
+defineExpose<ButtonInstance>({
+  ref:_ref,
+
+});
 </script>
 <template>
-  <button style="background-color: blue; color: red">this is a button</button>
+  <component
+    :is="tag"
+    ref="_ref"
+    class="er-button"
+     :disabled="disabled || loading ? true : void 0"
+    :type="tag === 'button' ? nativeType : void 0"
+    :class="{
+      [`er-button--${type}`]: type,
+      [`er-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+      'is-loading': loading,
+    }"
+    @click="(e:MouseEvent)=>useThrottle?handleBtnClickThrottle(e) : handleBtnClick"
+   
+  >
+    <slot></slot>
+  </component>
 </template>
+
+<style>
+@import './style.css'
+</style>
--- packages/components/vitest.config.ts
+++ packages/components/vitest.config.ts
@@ -13,3 +13,4 @@ export default defineConfig({
   },
 });
 
+// "test": "vitest --coverage"
