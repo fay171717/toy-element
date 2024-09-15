0ba9b59088a171cc9748a6d85b5fe45d4cdb7ee9
fay171717 <15777063880@163.com>
2024-09-15 17:22:12 +0800

    fixed

--- packages/docs/.vitepress/config.ts
+++ packages/docs/.vitepress/config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   title: "toy-element",
   description: "基于Vue3 高仿 element-ui 组件库",
   appearance: false, // 关闭 darkMode @todo 深色模式完成后打开
-  base: "/eric-ui/",
+  base: "/toy-element/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
